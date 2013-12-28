faa0bede6f446df5adf9b9624daedb06c3754e4f
Jildert Miedema <jildert@pimenta.nl>
2013-12-28 10:10:34 +0100

    initial commit

---
+++ src/Jildertmiedema/LaravelPlupload/LaravelPluploadServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Jildertmiedema\LaravelPlupload;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPluploadServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
