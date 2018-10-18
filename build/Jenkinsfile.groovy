5c7a97f382fe2012fdfa018f60c35ecf4924ad54
jildertmiedema <git@jildertmiedema.nl>
2018-10-18 17:45:51 +0200

    moved jenkinsfile

--- build/Jenkinsfile.groovy
+++ build/Jenkinsfile.groovy
@@ -13,7 +13,6 @@ pipeline {
                         sh 'php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');"'
                         sh 'php composer-setup.php'
                         sh 'php -r "unlink(\'composer-setup.php\');"'
-                        sh 'cp composer.phar composer.phar'
                     }
                 }
             }
